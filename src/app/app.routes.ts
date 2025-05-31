8a82a2a6be8cf976e6f16445cd5d4c7cf741c89a
Eugen1188 <eugen.ferchow@gmx.de>
2025-06-01 01:29:18 +0200

    add Herosection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingHeroComponent } from './landing-hero/landing-hero.component';
+import { ProduktlisteComponent } from './produktliste/produktliste.component';
 
 export const routes: Routes = [
     { path: '', component: LandingHeroComponent },
+    { path: 'products', component: ProduktlisteComponent}
 ];
