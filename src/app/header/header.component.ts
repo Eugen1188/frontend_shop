010a4532e89f46ddbe9a3faa65a7c36b91751680
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-09-07 00:45:51 +0300

    filters

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
+import { SharedService } from '../shared.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, SearchbarComponent],
+  imports: [RouterLink, SearchbarComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  show = false;
-  hello() {
-    this.show = true;
+  show = inject(SharedService);
+  open() {
+    this.show.show = true;
   }
 }
--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-searchbar',
   imports: [],
   templateUrl: './searchbar.component.html',
-  styleUrl: './searchbar.component.scss'
+  styleUrl: './searchbar.component.scss',
 })
 export class SearchbarComponent {
+  show = inject(SharedService);
 
+  close() {
+    if ((this.show.show = true)) {
+      this.show.show = false;
+    } else {
+      return;
+    }
+  }
 }
