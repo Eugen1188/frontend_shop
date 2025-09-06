f56b62290edbfc44db7b0e6e08c6d2047695c3f4
Eugen1188 <eugen.ferchow@gmx.de>
2025-09-07 01:22:15 +0200

    change poduktliste display

--- src/app/produktliste/produktliste.component.ts
+++ src/app/produktliste/produktliste.component.ts
@@ -6,7 +6,7 @@ import { FiltersComponent } from '../filters/filters.component';
 
 @Component({
   selector: 'app-produktliste',
-  imports: [CommonModule, ProduktPreviewComponentComponent,FiltersComponent],
+  imports: [CommonModule, ProduktPreviewComponentComponent, FiltersComponent],
   templateUrl: './produktliste.component.html',
   styleUrl: './produktliste.component.scss',
 })
@@ -14,6 +14,11 @@ export class ProduktlisteComponent implements OnInit {
   products: Product[] = [];
   categories: Category[] = [];
   private backendService = inject(BackendService);
+
+  pageSize = 16;
+  currentPage = 0;
+  loadedProducts: Product[] = [];
+
 
   ngOnInit(): void {
     this.backendService.getCategories().subscribe((data) => {
@@ -23,6 +28,30 @@ export class ProduktlisteComponent implements OnInit {
     this.backendService.getProducts().subscribe((data) => {
       this.products = data;
       console.log(this.products);
+      this.updateProducts();
     });
+  }
+
+    updateProducts() {
+    const start = this.currentPage * this.pageSize;
+    this.loadedProducts = this.products.slice(start, start + this.pageSize);
+  }
+
+  nextPage() {
+    if ((this.currentPage + 1) * this.pageSize < this.products.length) {
+      this.currentPage++;
+      this.updateProducts();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.updateProducts();
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.products.length / this.pageSize);
   }
 }
